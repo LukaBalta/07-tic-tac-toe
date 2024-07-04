b2fc4d421eda2fdf428276a7719cdc692d6d3789
LukaBalta <luka.balta.apple@gmail.com>
2024-07-04 17:52:10 +0200

    feat: created GameBoard component and lifted states to App

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -10,20 +11,35 @@ export default function Player({ name, symbol }) {
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
-
-  //   let buttonState = "Edit";
-
-  if (isEditing) {
-    playerName = <input type="text" required value={name} />;
-    //   buttonState = "Save";
+  function handleKeyPress(e) {
+    if (event.key === "Enter") {
+      setIsEditing(false);
+    }
   }
 
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+        onKeyDown={("enter", handleKeyPress)}
+      />
+    );
+  }
+  console.log(playerName);
   console.log(isEditing);
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
         <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </span>
